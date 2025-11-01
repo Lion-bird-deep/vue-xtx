d89536f6327c5fbdab133553d2616076994a838f
YangLei <2656190248@qq.com>
2025-11-01 17:59:49 +0800

    :min='0'

--- src/views/CartList/index.vue
+++ src/views/CartList/index.vue
@@ -54,7 +54,7 @@ const allCheck = (selected) => {
                 <p>&yen;{{ i.price }}</p>
               </td>
               <td class="tc">
-                <el-input-number v-model="i.count" />
+                <el-input-number v-model="i.count" :min="0" />
               </td>
               <td class="tc">
                 <p class="f16 red">&yen;{{ (i.price * i.count).toFixed(2) }}</p>
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -127,7 +127,7 @@ const addCart = () => {
               <!-- sku组件 -->
               <XtxSku :goods="goods" @change="skuChange" />
               <!-- 数据组件 -->
-              <el-input-number v-model="count" @change="countChange" />
+              <el-input-number v-model="count" @change="countChange" :min="0" />
               <!-- 按钮组件 -->
               <div>
                 <el-button size="large" class="btn" @click="addCart">
